e71b3258568e2e1e5b14b84ff3f13855236b9c6b
Alejandra <lilliana220393@gmail.com>
2025-10-15 22:27:05 -0600

    Creacion de Proyecto

---
+++ vistas/vista_superior.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="Spanish">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Proyecto Universitario">
+    <title>Tercera 1</title>
+  </head>
+  <body>
+    <h1>Bienvenida a tu proyecto</h1>
+  </body>
+</html>
+
