9cd5c4713fd65e05da25ef38abefaf3bb1567ca5
Alejandra <lilliana220393@gmail.com>
2025-10-22 23:17:14 -0600

    Avance de las cards

--- script/cards.js
+++ script/cards.js
@@ -15,30 +15,35 @@ const dataCards = [
         "cta": "Mostrar más...",
         "link": "./computadoras.php"
     },
-    {
-        "title": "Tablets",
-        "url_image": "./img/Tablets.jpg",
-        "desc": "Contamos con una amplia variedad de tablets para todo tipo de usuario.",
-        "cta": "Mostrar más...",
-        "link": "./tablets.php"
-    }
 {
-        "title": "Smartwatch",
-        "url_image": "./img/Smartwach.png",
-        "desc": "Gran variedad de smartwatches para todo tipo de usuario.",
-        "cta": "Mostrar más...",
-        "link": "./smartwatch.php"
-    }
-
-       {
+  "title": "Tablets",
+  "url_image": "./img/Tablets.jpg",
+  "desc": "Contamos con una amplia variedad de tablets para todo tipo de usuario.",
+  "cta": "Mostrar más...",
+  "link": "./tablets.php"
+}, 
+{
+  "title": "Smartwatch",
+  "url_image": "./img/Smartwach.png",
+  "desc": "Gran variedad de smartwatches para todo tipo de usuario.",
+  "cta": "Mostrar más...",
+  "link": "./smartwatch.php"
+},
+{
         "title": "Accesorios",
         "url_image": "./img/Accesorios.jpg",
         "desc": "Muchos accesosrios para complementar tus dispositivos.",
         "cta": "Mostrar más...",
         "link": "./accesorios.php"
+    },
+{
+        "title": "Quienes somos",
+        "url_image": "./img/Redes.jpg",
+        "desc": "Conoce más sobre nosotros y nuestras redes sociales.",
+        "cta": "Mostrar más...",
+        "link": "./contacto.php"
     }
 
-    
 ];
 
 (function () {
@@ -56,14 +61,15 @@ const dataCards = [
         },
 
         tplCardItem: function(item, index) {
-            return(`<div class='card-item id='card-number-${index}'>
+           return(`<div class='card-item' id='card-number-${index}'>
                 <img src='${item.url_image}'>
                 <div class='card-info'>
                     <p class='card-title'>${item.title}</p>
                     <p class='card-desc'>${item.desc}</p>
-                    <a class='card-cta' target='blank' href='${item.link}'>${item.cta}</a>
+                    <a class='card-cta' target='_blank' href='${item.link}'>${item.cta}</a>
                 </div>
-            </div>`)
+                </div>`);
+
         },
     }
     CARD.init();
